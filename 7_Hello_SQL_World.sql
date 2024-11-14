4220821ea428c3f6fca0523c859383c0fc13f8aa
AdrianProczek <adrian.proczek@gmail.com>
2024-11-14 17:45:14 +0100

    Complete Task Hello SQL World

---
+++ 7_Hello_SQL_World.sql
@@ -0,0 +1,9 @@
+-- 7. Hello SQL World!
+
+-- Hello SQL!
+
+-- Return a table with a single column named Greeting with the phrase 'hello world!'
+
+-- Please use Data Manipulation Language and not Data Definition Language to solve this Kata
+
+select 'hello world!' as "Greeting"
